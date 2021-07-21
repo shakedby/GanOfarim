0e6514fd40d215f7f2de8a8d7cf35875577f7f25
shaked <by181996@gmail.com>
2021-07-21 15:14:37 +0300

    commit

--- ganofarim/src/Components/About/About.js
+++ ganofarim/src/Components/About/About.js
@@ -1,1 +1,13 @@
+import React from 'react';
 import './About.css';
+function About() {
+    return (
+        <section id="About">
+            <h2>...קצת על הגן</h2>
+        </section >
+    )
+
+}
+export default About;
+
+
