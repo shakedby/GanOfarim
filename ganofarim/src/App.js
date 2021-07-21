32524bae2f1fb2e8627f65eccca4383711d08f35
shaked <by181996@gmail.com>
2021-07-21 15:14:25 +0300

    commit

--- ganofarim/src/App.js
+++ ganofarim/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import About from './Components/About/About';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
 
     </div>
   );
