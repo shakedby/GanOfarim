7a90ff597c217187381240e3b1694810a5596a4d
shaked ben yechezkel <76395111+shakedby@users.noreply.github.com>
2021-10-12 17:32:33 +0300

    12/10

--- ganofarim/src/Components/GalleryAdmin/GalleryAdmin.js
+++ ganofarim/src/Components/GalleryAdmin/GalleryAdmin.js
@@ -1,22 +1,34 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./GalleryAdmin.css";
 import { Redirect } from "react-router-dom";
 import Carousel from "react-gallery-carousel";
 import "react-gallery-carousel/dist/index.css";
+import axios from "axios";
 
 const GalleryAdmin = (props) => {
+  const [galleryList, setGallerylist] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/getallgal")
+      .then((res) => {
+        if (res.data.status === 404) {
+          console.log("error");
+        }
+        setGallerylist(res.data.gallerylist);
+      })
+      .catch(() => {
+        console.log("sucsees");
+      });
+  }, []);
+  //   console.log(galleryList);
+
   return (
     <div className="admin">
-      <Carousel
-        style={{
-          height: 500,
-          width: 800,
-          marginLeft: 360,
-          marginBottom: 100,
-          top: "155px",
-          backgroundColor: "orange",
-        }}
-      />
+      {galleryList.map((slide, indax) => {
+        console.log(slide.url);
+        return <img src={slide.url} alt="me" className="slider"></img>;
+      })}
     </div>
   );
 };
--- ganofarim/src/Components/GalleryAdmin/OneImg.js
+++ ganofarim/src/Components/GalleryAdmin/OneImg.js
@@ -5,14 +5,12 @@ const axios = require("axios");
 
 const OneImg = (props) => {
   function deleteImg() {
-    axios.delete("http://localhost:5000/Gallerytest");
+    axios.get("http://localhost:5000/api/Gallerytest/:id");
   }
-  function addImg() {}
 
   return (
     <div className="one-img">
       <button onClick={deleteImg}>מחיקת תמונה </button>
-      <button onClick={addImg}>הוספת תמונה </button>
     </div>
   );
 };
